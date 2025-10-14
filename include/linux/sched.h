405e582aed1fa6c8bebff0a119860531c435254b
starsea <mmdlsu@163.com>
2025-10-14 06:27:08 -0400

    add susfs 1.5.11 and hide_stuff.patch

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1403,6 +1403,9 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
+#ifdef CONFIG_KSU_SUSFS
+	u64 susfs_task_state;
+#endif
 	randomized_struct_fields_end
 
 	atomic64_t cpu_dist[8];
